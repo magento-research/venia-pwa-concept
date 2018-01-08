3be016db98b41f3e19e698592a44ac58f2763f0e
jimbo <jimbo@users.noreply.github.com>
2018-01-08 15:59:05 -0700

    Update error message for extract utility

--- theme/src/utils.js
+++ theme/src/utils.js
@@ -2,5 +2,5 @@ export const extract = (obj, name = 'default') =>
     Promise.resolve(obj)
         .then(mod => mod[name])
         .catch(() => {
-            throw new Error(`Export ${name} not found.`);
+            throw new Error(`Object is not a valid module.`);
         });
