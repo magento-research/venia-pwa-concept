ad43006e872c4975956ec645a4841a38775da58f
jimbo <jimbo@users.noreply.github.com>
2018-01-19 11:06:38 -0700

    Revert unintended change

--- theme/src/utils.js
+++ theme/src/utils.js
@@ -2,6 +2,5 @@ export const extract = (obj, name = 'default') =>
     Promise.resolve(obj)
         .then(mod => mod[name])
         .catch(error => {
-            console.error(error);
-            // throw new Error(`Object is not a valid module.`);
+            throw new Error(`Object is not a valid module.`);
         });
