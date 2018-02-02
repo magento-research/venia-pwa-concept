5ccf228855ac6103c5a25cf6e76517e614dad7d3
zetlen <zetlen@gmail.com>
2018-02-02 07:54:04 -0600

    fix(test): make get-magento-env test less dependent on wording

--- theme/lib/__tests__/get-magento-env.spec.js
+++ theme/lib/__tests__/get-magento-env.spec.js
@@ -23,10 +23,7 @@ test('sets defaults on fetch', () => {
     );
 });
 test('returns a rejected Promise with an error if called with no arg', () =>
-    expect(getMagentoEnv()).rejects.toHaveProperty(
-        'message',
-        expect.stringContaining('No Magento domain specified')
-    ));
+    expect(getMagentoEnv()).rejects.toHaveProperty('message'));
 test('calls fetch with the Magento wpconfig endpoint of the domain', () =>
     getMagentoEnv('https://example.com').then(env => {
         expect(fetch).toHaveBeenCalledWith(
