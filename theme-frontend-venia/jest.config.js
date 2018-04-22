a97317b5d59fa15f4d1187f6bbd24641fb7b6154
zetlen <zetlen@gmail.com>
2018-04-22 10:20:25 -0700

    fix: remove devtools

--- theme-frontend-venia/jest.config.js
+++ theme-frontend-venia/jest.config.js
@@ -10,20 +10,6 @@ module.exports = {
                 '\\.css$': 'identity-obj-proxy',
                 '^src/(.+)': '<rootDir>/src/$1'
             }
-        },
-        {
-            displayName: 'devtools',
-            testMatch: ['<rootDir>/devtools/**/__tests__/*.spec.js'],
-            browser: false,
-            testEnvironment: 'node',
-            collectCoverageFrom: ['devtools/**/*.js'],
-            moduleNameMapper: {
-                // There is no `src` dir or `.css` files in the `devtools`
-                // dir, so these are a noop. But they're circumventing a bug in
-                // Jest's multi-project runner
-                '\\.css$': 'identity-obj-proxy',
-                '^src/(.+)': '<rootDir>/src/$1'
-            }
         }
     ]
 };
